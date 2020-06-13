9027f99accbf2a8190227c297756b2570b0c05c5
Deer5000 <khidrbrinkley32@gmail.com>
2020-06-13 17:16:30 -0400

    root,app url's, and views done

--- portfolio/portfo/views.py
+++ portfolio/portfo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.template import loader
 
-# Create your views here.
+
+
+def home(request):
+    return HttpResponse("Portfolio Home")
+
+def contact(request):
+    return HttpResponse("Contact Me")
